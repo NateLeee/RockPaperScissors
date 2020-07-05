495841f0499ada27e8b5351db00198429ae99b23
Nate Lee <nateleee.dev@gmail.com>
2020-07-05 15:29:09 +0800

    Looking okay right now.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,41 @@
 
 import SwiftUI
 
+enum GameRule: Int {
+    case ToWin = 0
+    case ToLose
+}
+
 struct ContentView: View {
+    static let moves = ["Rock", "Paper", "Scissors"]
+    
+    @State private var currentMove: String = moves[Int.random(in: 0..<3)]
+    @State private var rule = GameRule(rawValue: Int.random(in: 0...1))
+    @State private var score: Int = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 27) {
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Score: \(score)")
+                Text("Computer Move: \(currentMove)")
+                Text("And you should: \(rule == .ToWin ? "WIN" : "LOSE")!")
+            }
+            
+            HStack {
+                ForEach(ContentView.moves, id: \.self) { move in
+                    Button(action: {
+                        // TODO: - Implement this logic
+                        print(move)
+                    }) {
+                        Text(move)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                    }
+                }
+            }
+            
+        }
     }
 }
 
